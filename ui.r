40aeb8a95457a433a9971d01f7978a9d16d93fff
timelyportfolio <kent.russell@timelyportfolio.com>
2012-12-17 21:55:10 -0600

    couple changes extending the description in ui.r

--- ui.r
+++ ui.r
@@ -2,9 +2,11 @@ shinyUI(pageWithSidebar(
   headerPanel('Downloading Data with Shiny'),
   sidebarPanel(
     helpText("An Example of R creating a pdf and then delivering with Shiny to Download.  This 
-            is a very crude example in serious need of refinement.  Shiny has prebuilt functions
-            for delivering R plots through .png.  Ideally, we could use knitr knit2pdf to
-            build a pdf and then send it."),
+            is a very crude example in serious need of refinement and should be considered
+            highly experimental.  Shiny has prebuilt functions with plotOutput
+            for delivering R plots through .png, but sometimes we might like to deliver more than .png.
+            Ideally, we could use knitr's knit2pdf function to access the power of latex
+            to build a multi-page pdf real-time and then send it to the browser."),
     downloadLink('downloadPdf', 'Download Performance File')
   ),
   mainPanel(
